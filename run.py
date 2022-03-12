bc77d3c8c82698c68cf57115194ef214363873e0
mattm1346 <MATTM1346@gmail.com>
2022-03-12 12:45:20 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
-# python code goes here
+# Print board function
+
+    # Player Board (hidden)
+
+# Computer create ships
+
+# Guess ship location
+
+# Check if all ships are hit
+
+# Run Game function
