055c5856276ce020bac9ad53602edc663e248faf
mattm1346 <MATTM1346@gmail.com>
2022-03-13 10:05:57 +0000

    Fix issue allowing empty fields

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from random import randint
 
 # Player Board (hidden)
-PLAYER_BOARD = [[' '] * 8 for x in range(8)]
-COMPUTER_BOARD = [[' '] * 8 for i in range(8)]
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+SHOWN_BOARD = [[' '] * 8 for i in range(8)]
     
 
 # Convert letters to numbers
@@ -16,6 +16,8 @@ letters_to_numbers = {
     'G': 6,
     'H': 7
 }
+
+
 # Print board
 def print_board(board):
     print("  A B C D E F G H")
@@ -25,6 +27,7 @@ def print_board(board):
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
+
 # Computer create ships
 def create_ships(board):
     for ship in range(5):
@@ -33,18 +36,28 @@ def create_ships(board):
             ship_row, ship_column = locate_ships()
         board[ship_row][ship_column] = "X"
 
+
 # Guess ship location
 def locate_ships():
     # Player guesses ship location
-    row = input("Enter the row of the ship (1-8): \n")
-    while row not in "12345678":
-        print('Error, please select a valid row')
-        row = input("Enter the row of the ship (1-8): \n")
-    column = input("Enter the column of the ship (A-H): \n").upper()
-    while column not in "ABCDEFGH":
-        print('Error, please select a valid column')
-        column = input("Enter the column of the ship (A-H): \n").upper()
-    return int(row) - 1, letters_to_numbers[column]
+    while True:
+        try: 
+            row = input("Enter the row of the ship between 1 and 8: \n")
+            if row in '12345678':
+                row = int(row) - 1
+                break
+        except ValueError:
+            print('Error, please enter a valid number between 1 and 8')
+    while True:
+        try: 
+            column = input("Enter the column of the ship between A and H: \n").upper()
+            if column in 'ABCDEFGH':
+                column = letters_to_numbers[column]
+                break
+        except KeyError:
+            print('Error, please enter a valid letter between A-H')
+    return row, column
+
 
 # Check if all ships are hit
 def count_hits(board):
@@ -55,28 +68,31 @@ def count_hits(board):
                 count += 1
     return count
 
+
 # Run Game function
-create_ships(PLAYER_BOARD)
-turns = 10
-while turns > 0:
-    print('Welcome to Battleships')
-    # Print computer board so player has idea of where to guess
-    print_board(COMPUTER_BOARD)
-    row, column = locate_ships()
-    # Check if player guesses in same place
-    if COMPUTER_BOARD[row][column] == "-":
-        print("Error, You guessed that one already.")
-    elif PLAYER_BOARD[row][column] == "X":
-        print("Hit")
-        COMPUTER_BOARD[row][column] = "X" 
-        turns -= 1
-    else:
-        print("MISS!")
-        PLAYER_BOARD[row][column] = "-"   
-        turns -= 1
-    if count_hits(COMPUTER_BOARD) == 5:
-        print("You win!")
-        break
+if __name__ == "__main__":
+    create_ships(HIDDEN_BOARD)
+    turns = 10
+    while turns > 0:
+        print('Welcome to Battleships')
+        # Print computer board so player has idea of where to guess
+        print_board(SHOWN_BOARD)
+        row, column = locate_ships()
+        # Check if player guesses in same place
+        if SHOWN_BOARD[row][column] == "-":
+            print("Error, You guessed that one already.")
+        elif HIDDEN_BOARD[row][column] == "X":
+            print("HIT")
+            SHOWN_BOARD[row][column] = "X" 
+            turns -= 1
+        else:
+            print("MISS!")
+            SHOWN_BOARD[row][column] = "-"   
+            turns -= 1
+        if count_hits(SHOWN_BOARD) == 5:
+            print("CONGRATULATIONS, YOU WIN!")
+            break
         print("You have " + str(turns) + " turns left")
-    if turns == 0:
-        print("You ran out of turns")
+        if turns == 0:
+            print("You ran out of turns")
+            print_board(HIDDEN_BOARD)
